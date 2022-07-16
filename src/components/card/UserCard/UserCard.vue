d00b87443dd83fc76e0d982cdbd1031459e5e2c9
Egor <egor.uxanov.97@mail.ru>
2022-07-16 14:13:30 +0300

    Карточка пользователя
    1. Валидация пароля
    2. Обновили стили

--- src/components/card/UserCard/UserCard.vue
+++ src/components/card/UserCard/UserCard.vue
@@ -20,7 +20,7 @@
         beforeMount(): void {
             this.localData = this.data;
         },
-        data() {
+        data(): {[key: string]: any} {
             return {
                 rules: {
                     name: [
@@ -40,11 +40,12 @@
                         { min: 5, max: 40, message: 'Длинна должна быть от 4 до 20 символов', trigger: 'blur' },
                     ],
                     password: [
-                        // { required: true, validator: validatePass, message: 'Введите пароль', trigger: 'blur' },
+                        { required: true, message: 'Введите пароль', trigger: 'blur' },
                         { min: 5, max: 40, message: 'Длинна должна быть от 8 до 20 символов', trigger: 'blur' },
                     ],
                     checkPass: [
-                        // {required: true, validator: validatePass2, message: 'Подтвердите пароль', trigger: 'blur'},
+                        {required: true, message: 'Введите подтверждение пароля', trigger: 'blur'},
+                        {validator: this.passwordConfirmValidator, trigger: 'blur'}
                     ],
                     email: [
                         { required: true, message: 'Введите почту', trigger: 'blur' },
@@ -64,6 +65,15 @@
             },
             cardSave() {
                 this.$emit('cardSave', this.localData);
+            },
+            passwordConfirmValidator(rule: any, value: any, callback: any) {
+                if (value === '') {
+                    callback(new Error('Поле подтверждения не заполнено'))
+                } else if (value !== this.localData.password) {
+                    callback(new Error("Пароли не совпадают"))
+                } else {
+                    callback()
+                }
             }
         },
         computed: {
@@ -80,93 +90,7 @@
             }
         }
     });
-
-    import { ref } from 'vue'
-    import { ElMessage } from 'element-plus'
-    import { Plus } from '@element-plus/icons-vue'
-
-    import type { UploadProps } from 'element-plus'
-
-    const imageUrl = ref('')
-
-    const handleAvatarSuccess: UploadProps['onSuccess'] = (
-        response,
-        uploadFile
-    ) => {
-        imageUrl.value = URL.createObjectURL(uploadFile.raw!)
-    }
-
-    const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
-        if (rawFile.type !== 'image/jpeg') {
-            ElMessage.error('Аватар должен быть JPG формата!')
-            return false
-        } else if (rawFile.size / 1024 / 1024 > 2) {
-            ElMessage.error('Аватар должен весить 2MB!')
-            return false
-        }
-        return true
-    }
-
-    // const validatePass = (rule: any, value: any, callback: any) => {
-    //     if (value === '') {
-    //         callback(new Error('Пожалуйста введите пароль'))
-    //     } else {
-    //         if (ruleForm.checkPass !== '') {
-    //             // eslint-disable-next-line no-undef
-    //             if (!ruleFormRef.value) return
-    //             // eslint-disable-next-line no-undef
-    //             ruleFormRef.value.validateField('checkPass', () => null)
-    //         }
-    //         callback()
-    //     }
-    // }
-    // const validatePass2 = (rule: any, value: any, callback: any) => {
-    //     if (value === '') {
-    //         callback(new Error('Пожалуйста введите пароль снова'))
-    //     } else if (value !== ruleForm.pass) {
-    //         callback(new Error("Пароли различаются"))
-    //     } else {
-    //         callback()
-    //     }
-    // }
-
-    // eslint-disable-next-line no-undef
-    // const ruleForm = reactive({
-    //     pass: '',
-    //     checkPass: '',
-    // })
-
 </script>
 
 
-<style scoped lang="less" src="./style.less">
-    .avatar-uploader .avatar {
-        width: 178px;
-        height: 178px;
-        display: block;
-    }
-</style>
-
-<style>
-    .avatar-uploader .el-upload {
-        border: 1px dashed var(--el-border-color);
-        border-radius: 6px;
-        cursor: pointer;
-        position: relative;
-        overflow: hidden;
-        transition: var(--el-transition-duration-fast);
-    }
-
-    .avatar-uploader .el-upload:hover {
-        border-color: var(--el-color-primary);
-    }
-
-    .el-icon.avatar-uploader-icon {
-        font-size: 28px;
-        color: #8c939d;
-        width: 178px;
-        height: 178px;
-        text-align: center;
-    }
-
-</style>
+<style lang="less" src="./style.less"></style>
