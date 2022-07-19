ee79542b17300d5d29768723406f9b3c087d1613
Egor <egor.uxanov.97@mail.ru>
2022-07-19 18:42:15 +0300

    Карточка пользователя
    1. Добавлена маска для ввода номера телефона
    2. Добавлен дата пикер

---
+++ src/components/basic/PhoneNumber/PhoneNumber.vue
@@ -0,0 +1,32 @@
+<template src="./template.html"/>
+
+<script lang="ts">
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        name: 'PhoneNumberView',
+        props: {
+          value:{
+              type: String,
+              required: true
+          },
+        },
+        beforeMount(): void {
+            this.number = this.value || '+7  ';
+        },
+        data: function () {
+            return {
+                number: '',
+            }
+        },
+        methods: {
+            numberChange() {
+                this.number = this.number.replace(/[^0-9]/g, '')
+                    .replace(/^\+7(\d{3})?(\d{3})?(\d{4})?/g, '+7 ($1) $2-$3')
+                this.$emit('numberChange', this.number);
+            }
+        }
+    });
+</script>
+
+<style scoped lang="less" src="./style.less"></style>
